c6cc081bbeca02d02e739e2d2fff2e472f92bd6e
Christian Zei <christian.zei@stud.tu-darmstadt.de>
2020-06-19 14:50:35 +0200

    add char pairs to config file

--- data_study.py
+++ data_study.py
@@ -4,7 +4,7 @@ import key_pair_generator as keygen
 
 task_gen = tasks()
 task_gen.welcome_task()
-task_gen.task_1(keygen.get_char_pairs())
+task_gen.task_1(config.key_pairs)
 #task_gen.task_2(keygen.get_shift_pairs())
 #task_gen.task_3(config.words)
 task_gen.task_4(config.passwords)
